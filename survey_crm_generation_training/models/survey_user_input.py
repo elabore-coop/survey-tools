4be0f29c28899d68f2ea567a590974526b5a0170
clementthomas <clement@cotyl.net>
2024-07-10 13:28:11 +0200

    [IMP] survey_crm_generation_training: use user_id of survey in created new lead

--- survey_crm_generation_training/models/survey_user_input.py
+++ survey_crm_generation_training/models/survey_user_input.py
@@ -1,15 +1,22 @@
 # Copyright 2022 Tecnativa - David Vidal
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from odoo import fields, models, _
+from odoo import fields, models, _, Command
 
 
 class SurveyUserInput(models.Model):
     _inherit = "survey.user_input"
 
-    def _create_opportunity_post_process(self):
-        """After lead creation from survey answer (module survey_crm_generation), 
-        if answer (survey_user_input) contains event_type_ids, copy it in lead.
-        """
-        self.opportunity_id.event_type_ids = self.event_type_ids.ids
+    def _prepare_opportunity(self):
+        res = super(SurveyUserInput, self)._prepare_opportunity()
+        
+        #if answer (survey_user_input) contains event_type_ids, copy it in lead.
+        res["event_type_ids"] = [Command.set(self.event_type_ids.ids)]
 
-        return super(SurveyUserInput, self)._create_opportunity_post_process()
+
+        # use survey responsible as seller instead of manager of sale department
+        if self.survey_id.user_id:
+            res["user_id"] = self.survey_id.user_id.id
+
+
+        return res
+
